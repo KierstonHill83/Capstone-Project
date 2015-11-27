6d8e329cb12d9e513ff7f91cfd34214d434b302e
KierstonHill83 <khill1218@gmail.com>
2015-11-26 17:53:09 -0700

    Added routes on client side

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,94 @@
-// add scripts
+// // custom scripts
 
-$(document).on('ready', function() {
-  console.log('sanity check!');
+var socket = io();
+
+
+$('#chatForm').hide();
+
+// Grab the value of the initial user name.
+$('#socketform').submit(function() {
+  socket.emit('setName',$('#m').val());
+  console.log('name ' +$('#m').val());
+  $('#m').val('');
+  $('#socketform').hide();
+  return false;
+});
+
+// Grab the value of the person they want to invite.
+$('#privateForm').submit(function() {
+    socket.emit('createRoom', $('#p').val(), $('#r').val());
+    console.log('name '+$('#p').val());
+    console.log('room ' +$('#r').val());
+    $('#p').val('');
+    $('#r').val('');
+    $('#chatForm').show();
+    $('#privateForm').hide();
+    return false;
+});
+
+// Grab the value of the message that is being sent.
+$('#chatForm').submit(function() {
+    socket.emit('chat message', $('#t').val());
+    $('#t').val('');
+    return false;
+});
+
+// Append the join me message link to the screen.
+socket.on('privateChat', function(msg) {
+    $('#message').append($('<li>').html(msg));
+});
+
+// Console log the messsage
+socket.on('private', function(msg) {
+    console.log(msg);
+});
+
+// Apped the message to the screen.
+socket.on('chat message', function(msg) {
+  $('#message').append($('<li>').text(msg));
+});
+
+
+
+////////////////////
+// user info form //
+////////////////////
+
+$('#user-signup').submit(function() {
+  $.post('/api/users', {
+    name: $('#first-name').val(),
+    username: $('#username').val(),
+    email: $('#email').val(),
+    location: $('#location').val(),
+    gender: $('#gender').val(),
+    age: $('#age').val(),
+    password: $('#password').val()
+  },
+  function(data, status) {
+    console.log('data from callback: ' + data);
+    console.log('status from callback: ' + status);
+  });
+});
+
+
+
+////////////////////////
+// user activity form //
+////////////////////////
+
+
+$('#user-activity').submit(function() {
+  console.log($('.activity-option').val());
+console.log($('.activity-name').val());
+console.log($('.activity-value').val());
+
+  $.post('/api/userActivities', {
+    userActivity: $('.activity-option').val(),
+    propertyName: $('#activity-name').val(),
+    propertyValue: $('#activity-value').val()
+  },
+  function(data, status) {
+    // console.log('data ' + data);
+    console.log('status ' + status);
+  });
 });
